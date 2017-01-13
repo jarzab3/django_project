1755f9316c3427a1e03e2726d4eddcad6d02d3c9
Adam Jarzebak <adam@expensecheck.comm.au>
2017-01-14 01:09:20 +1100

    Clean up

--- regression/models.py
+++ regression/models.py
@@ -11,21 +11,3 @@ class UserStory(models.Model):
 
 class EmailCategory(models.Model):
     key = models.CharField(max_length=200)
-
-
-'''
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-
-    def __unicode__(self):
-        return self.title
-
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse("posts:detail", kwargs={"id": seld.id})
-
-
-'''

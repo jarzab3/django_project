1e4bcc5e57a3f30729a13a15155105b7916b743f
Adam Jarzebak <adam@expensecheck.comm.au>
2017-01-14 01:06:41 +1100

    Updated url links, refactored names

--- main_project/urls.py
+++ main_project/urls.py
@@ -7,13 +7,17 @@ from regression import forms
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^index.html$', views.index, name='index'),
+    url(r'^$', views.main, name='main'),
+    url(r'^main$', views.main, name='main'),
+    url(r'^create$', views.user_story_post_create, name='us_post_create'),
+    url(r'^display_us$', views.display_us_subject, name='display_us_subject'),
+    
+    #Below unused
+    
     url(r'^display_us/(?P<pk>\d+)/', views.user_story_detail_view, name='display_us_details'),
     url(r'^tables$', views.tables, name='tables'),
-    url(r'^display_us$', views.display_us_subject, name='display_us_subject'),
     #url(r'^item/(?P<id>\d+)/', views.item_detail, name='item_detail'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^form_upload.html$', views.submitted, name='subject_to_change'),
-    url(r'^create$', views.post_create, name='post_create'),
+    url(r'^submitted$', views.submitted, name='subject_to_change'),
+    
     ]

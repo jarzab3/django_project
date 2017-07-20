c147563b7c8ba332b569eddc5099b1913c5d4198
Adam Jarzebak <adam@jarzebak.eu>
2017-07-20 19:01:20 +1000

    Fix add new US, add new field 'created', ad logging

--- main_project/urls.py
+++ main_project/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf.urls import patterns, url
+# from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 from regression import views

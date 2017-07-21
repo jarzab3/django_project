9ab6bf50c8c897a6585de6d69cffd9bf3f71d557
Adam Jarzebak <adam@jarzebak.eu>
2017-07-21 20:38:54 +1000

    Remove old unused stuff

--- regression/views.py
+++ regression/views.py
@@ -14,18 +14,9 @@ from regression.models import UserStory
 
 logger = logging.getLogger(__name__)
 
-
-# def main(request):
-#     return render(request, 'regression/index.html', {
-#     })
-
-from regression.forms import MessageForm
-
 def main(request):
-    # This view is missing all form handling logic for simplicity of the example
-    return render(request, 'regression/index.html', {'form': MessageForm()})
-
-
+    return render(request, 'regression/index.html', {
+    })
 
 def submitted(request):
     return render(request, 'regression/submitted.html', {

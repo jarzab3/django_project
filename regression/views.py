5da6d611a4cf4ea32534c7bd01522bb1a67acda4
Adam Jarzebak <adam@jarzebak.eu>
2017-07-22 16:20:00 +1000

    Remove unused files, finish login feature, WIP frontend for login feature needs to be improve

--- regression/views.py
+++ regression/views.py
@@ -30,35 +30,7 @@ from . import views
 def home(request):
     return render(request, "index.html")
 
-
-# def login(request):
-#     return render(request, 'regression/login.html', {
-#     })
-#
-# def main(request):
-#     return render(request, 'regression/index.html', {
-#     })
-
-
-
-
-# def submitted(request):
-#     return render(request, 'regression/submitted.html', {
-#     })
-#
-# def forms(request):
-#     return render(request, 'regression/forms.html', {
-#     })
-
-#
-# def tables(request):
-#     return render(request, 'regression/tables.html', {
-#     })
-#
-# def charts(request):
-#     return render(request, 'regression/charts.html', {
-#     })
-
+@login_required(login_url="login/")
 def display_us_subject(request):
     try:
         all_user_stories = UserStory.objects.all()
@@ -73,6 +45,7 @@ def display_us_subject(request):
 
 
 @csrf_exempt
+@login_required(login_url="login/")
 def user_story_post_create(request):
     """Create a new user story
 
@@ -106,10 +79,6 @@ def user_story_post_create(request):
     	"form": form, 
     	})
 
-
-def user_story_detail_view(request, id):
-    return HttpResponse('<p> In item_detail view with pk {0}</p>'.format(id))
-
 def modal_detail_view(request, id):
 	try:
 		user_story = UserStory.objects.get(id=id)
@@ -120,23 +89,27 @@ def modal_detail_view(request, id):
 	})
 
 
-def login_user(request):
-    logout(request)
-    username = password = ''
-    if request.POST:
-        username = request.POST['username']
-        password = request.POST['password']
+def user_story_detail_view(request, id):
+    return HttpResponse('<p> In item_detail view with pk {0}</p>'.format(id))
 
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return HttpResponseRedirect('/main/')
 
-    # return render_to_response('regression/login.html', context_instance=RequestContext(request))
 
-    return render(request, 'login.html', {
-        # 'data': "logged"
-    })
+# def login_user(request):
+#     logout(request)
+#     username = password = ''
+#     if request.POST:
+#         username = request.POST['username']
+#         password = request.POST['password']
 #
+#         user = authenticate(username=username, password=password)
+#         if user is not None:
+#             if user.is_active:
+#                 login(request, user)
+#                 return HttpResponseRedirect('/main/')
 #
+#     # return render_to_response('regression/login.html', context_instance=RequestContext(request))
+#
+#     return render(request, 'login.html', {
+#         # 'data': "logged"
+#     })
+
